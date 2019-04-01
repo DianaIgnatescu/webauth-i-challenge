8186981949b028bfabf86bd0da3ed4535f58c640
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-01 12:57:13 +0100

    MVP: Add helmet and test server

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const helmet = require('helmet');
 const knex = require('knex');
 const knexConfig = require('./knexfile').development;
 
 const db = knex(knexConfig);
 const server = express();
+
+server.use(helmet());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  res.send('I\'m ready to party!');
+});
 
 
-const port = 5000;
+const port = 4000;
 server.listen(port, () => console.log(`Listening on http://localhost:${port}!`));
