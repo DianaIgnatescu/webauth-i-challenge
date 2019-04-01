b6fb693dd8c674f44ff038ffafef6d7f3a276453
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-01 15:44:58 +0100

    MVP: Create session for user

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const knex = require('knex');
 const knexConfig = require('./knexfile').development;
 
 const db = knex(knexConfig);
-const bcrypt = require('bcryptjs');
+
+
+const session = require('express-session');
 const server = express();
+
+const sessionConfig = {
+  name: 'monkey',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 15,
+    secure: false,
+  },
+  httpOnly: true, // user can't access the cookie from js using document.cookie
+  resave: false,
+  saveUninitialized: false, // GDPR laws against setting cookies automatically
+};
 
 server.use(helmet());
 server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
   res.send('I\'m ready to party!');
@@ -56,6 +74,7 @@ server.post('/api/login', (req, res) => {
       .first()
       .then((user) =>{
         if( user && bcrypt.compareSync(password, user.password)) {
+          req.session.user = user;
           res.status(200).json({message: `Welcome ${user.username}!`});
         } else {
           res.status(401).json({ message: 'You shall not pass!'});
